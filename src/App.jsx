ea0e9a0697ab52f8e8f261b211edc616ac76a776
Naga Maruti Sankar <v.sunithasankar@gmail.com>
2024-11-18 17:37:22 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+    <h1>Day7</h1>
+    {/* TODO: 1.Implement the Portfolio application */}
+      {/* TODO: 2.Push the code to github */}
+      {/* TODO: 3.Deploy into Netlify */}
+    </>
+  )
+}
+
+export default App
